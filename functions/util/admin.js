65cc3840679d812783ca44b72756cd08f0bad26d
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-02-29 15:56:45 -0800

    #7 Refactoring & Organising

---
+++ functions/util/admin.js
@@ -0,0 +1,16 @@
+const admin = require("firebase-admin");
+
+// Need for local
+//let serviceAccount = require("/Users/dk/Documents/GitHub/classed-social-firebase-server/keys/serviceAccountKey.json");
+admin
+  .initializeApp
+  // Need for local
+  //   {
+  //     credential: admin.credential.cert(serviceAccount),
+  //     databaseURL: "https://social-573b5.firebaseio.com"
+  //   }
+  ();
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDWJ-dGgHVrhZ_XZy-PjogFDYT1PDZElZI",
+  authDomain: "social-573b5.firebaseapp.com",
+  databaseURL: "https://social-573b5.firebaseio.com",
+  projectId: "social-573b5",
+  storageBucket: "social-573b5.appspot.com",
+  messagingSenderId: "830494776989",
+  appId: "1:830494776989:web:2383a66a1b6384dfe07e74",
+  measurementId: "G-ZJC4FE4J9P"
+};

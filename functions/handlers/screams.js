2d9590ac21486f43ea1d7cf1fb5f467b1d0df3a5
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-03-03 17:47:51 -0800

    #13 Finishing up Cloud Functions

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -14,7 +14,8 @@ exports.getAllScreams = (request, response) => {
           userHandle: doc.data().userHandle,
           createdAt: doc.data().createdAt,
           commentCount: doc.data().commentCount,
-          likeCount: doc.data().likeCount
+          likeCount: doc.data().likeCount,
+          userImage: doc.data().userImage
         });
       });
       return response.json(screams);
@@ -81,7 +82,7 @@ exports.getScream = (req, res) => {
 // Comment on a comment
 exports.commentOnScream = (req, res) => {
   if (req.body.body.trim() === "")
-    return res.status(400).json({ error: "Must not be empty" });
+    return res.status(400).json({ comment: "Must not be empty" });
 
   const newComment = {
     body: req.body.body,
--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -64,7 +64,9 @@ exports.signup = (request, response) => {
       if (err.code === "auth/email-already-in-use") {
         return response.status(400).json({ email: "Email is already is use" });
       } else {
-        return response.status(500).json({ error: err.code });
+        return response
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
       }
     });
 };
@@ -90,12 +92,13 @@ exports.login = (request, response) => {
       return response.json({ token });
     })
     .catch(err => {
-      console.error(err);
-      if (err.code === "auth/wrong-password") {
-        return response
-          .status(403)
-          .json({ general: "Wrong credentials, please try again" });
-      } else return response.status(500).json({ error: err.code });
+      // console.error(err);
+
+      // auth/wrong-password
+      // auth/user-not-user
+      return response
+        .status(403)
+        .json({ general: "Wrong credentials, please try again" });
     });
 };
 
